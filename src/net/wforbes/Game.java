5b01678b71a7d6fd5c6a27a55769fdc61ccd8fe9
Will Forbes <wforbes@users.noreply.github.com>
2019-10-20 19:08:00 -0700

    initial commit with the start of the main class Game.java

---
+++ src/net/wforbes/Game.java
@@ -0,0 +1,27 @@
+package net.wforbes;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+
+    private Game()
+    {
+
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+
+    private synchronized void start()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        new Game().start();
+    }
+}
