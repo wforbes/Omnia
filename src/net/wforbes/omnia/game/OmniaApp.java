b3958de2351e34ba01fcc776af8616618371ac6e
Will Forbes <wforbes@users.noreply.github.com>
2021-01-01 03:18:37 -0800

    Update OmniaApp.java
    Did some testing in FXGL 'OmniaApp' class.. but reverted most changes for now.

--- src/net/wforbes/omnia/game/OmniaApp.java
+++ src/net/wforbes/omnia/game/OmniaApp.java
@@ -5,8 +5,18 @@ import com.almasb.fxgl.app.GameSettings;
 
 public class OmniaApp extends GameApplication {
 
-    @Override
-    protected void initSettings(GameSettings gameSettings) {
-
+    public static void main(String[] args) {
+        launch(args);
     }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1280);
+        settings.setHeight(720);
+    }
+
+    protected void onUpdate(double tpf) {
+        System.out.println("tick " + tpf);
+    }
+
 }
